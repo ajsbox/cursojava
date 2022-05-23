67e2ea6038f9b6d53d9bb977cdde9d0a09f9a607
Alfonso <santana.alfonso@gmail.com>
2022-05-22 22:13:19 -0400

    IncrementoDecremento

---
+++ cursojava/src/cursojava/IncrementoDecremento.java
@@ -0,0 +1,21 @@
+package cursojava;
+
+public class IncrementoDecremento {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		
+		System.out.println(a); // a es 5
+		a = a + 1;
+		System.out.println(a); // a se incrementa a 6
+		
+		a++; // Usando el operador unario
+		
+		System.out.println(a);
+		
+		a--;
+		
+		System.out.println(a);
+	}
+
+}
