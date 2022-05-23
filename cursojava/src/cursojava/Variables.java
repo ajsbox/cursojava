f59e830f46c52f55d7b1090ba5b0e38c5eda503d
Alfonso <santana.alfonso@gmail.com>
2022-05-22 21:35:00 -0400

    Variables

---
+++ cursojava/src/cursojava/Variables.java
@@ -0,0 +1,18 @@
+package cursojava;
+
+public class Variables {
+	public static void main(String[] args) {
+		
+		byte numeroByte = 127;
+		short numeroShort = 31000;
+		int numeroInt = 23;
+		long numeroLong = 9876542L;
+		float numeroFloat = 987747474F;
+		double numeroDouble = 9876543123D;
+		boolean datoBoleano = true;
+		char caracter = 't';
+
+		System.out.println(numeroByte);
+	}
+
+}
