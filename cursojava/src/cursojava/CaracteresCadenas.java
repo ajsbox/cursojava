2decde16dac5456428941504b8f90827d5f924a7
Alfonso <santana.alfonso@gmail.com>
2022-05-22 21:40:24 -0400

    Caracteres y Cadenas

---
+++ cursojava/src/cursojava/CaracteresCadenas.java
@@ -0,0 +1,11 @@
+package cursojava;
+
+public class CaracteresCadenas {
+	public static void main(String[] args) {
+		String nombre = "Pedro";
+		char caracter = '\u00F1'; // Caracter ñ
+		
+		System.out.println(caracter);
+	}
+
+}
