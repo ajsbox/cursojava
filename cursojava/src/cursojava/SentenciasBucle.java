3bd80a8638704c9b5a2d6e5282cc33a5e70423a9
Alfonso <santana.alfonso@gmail.com>
2022-05-22 22:45:53 -0400

    .

--- cursojava/src/cursojava/SentenciasBucle.java
+++ cursojava/src/cursojava/SentenciasBucle.java
@@ -1,5 +1,28 @@
 package cursojava;
 
 public class SentenciasBucle {
+	public static void main(String[] args) {
+		
+		int a = 0;
+		
+		// while
+		while (a < 5) {
+			System.out.println(a);
+			a++;
+		}
+		
+		// do-while
+		int i = 1;    
+	    do{    
+	        System.out.println(i);    
+	    i++;    
+	    } while(i<=10); 
+	    
+	    // for
+	    for (int x = 0; x < 5; x++) {
+	    	  System.out.println("Numero: " + x);;
+	    	}
+
+	}
 
 }
