9d5748abdb2679728e25d8f420770baa9e6d65f0
Alfonso <santana.alfonso@gmail.com>
2022-05-23 11:12:56 -0400

    .

--- cursojava/src/cursojava/SentenciaRamificaciones.java
+++ cursojava/src/cursojava/SentenciaRamificaciones.java
@@ -3,11 +3,24 @@ package cursojava;
 public class SentenciaRamificaciones {
 	public static void main(String[] args) {
 		
+		// Primer ejemplo
 		for(int i=1;i<=10;i++){  
 	        if(i==5){   
 	            continue;  
 	        }  
 	        System.out.println(i);  
 	    }  
+		
+		
+		// Segundo ejemplo
+		for(int i = 1; i <= 10; ++i) {
+
+		      // if value of i is between 4 and 9
+		      // continue is executed
+		      if (i > 4 && i < 9) {
+		        continue;
+		      }
+		      System.out.println(i);
+		}
 	}
 }
