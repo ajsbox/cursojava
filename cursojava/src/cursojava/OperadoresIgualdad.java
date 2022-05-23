87b4eb86fd6eaaecea9a1d424a1ed46094963faa
Alfonso <santana.alfonso@gmail.com>
2022-05-22 21:47:03 -0400

    Operadores

--- cursojava/src/cursojava/OperadoresIgualdad.java
+++ cursojava/src/cursojava/OperadoresIgualdad.java
@@ -1,5 +1,16 @@
 package cursojava;
 
 public class OperadoresIgualdad {
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 7;
+		
+		if(a > b) {
+			System.out.println("A es mayor");
+		}
+		if(a < b) {
+			System.out.println("B es mayor");
+		}
+	}
 
 }
