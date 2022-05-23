9d5a757c3d603c79f1a6a2a3c38e4ee07d431edc
Alfonso <santana.alfonso@gmail.com>
2022-05-22 21:54:45 -0400

    Condicionales

---
+++ cursojava/src/cursojava/OperadoresCondicionales.java
@@ -0,0 +1,22 @@
+package cursojava;
+
+public class OperadoresCondicionales {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 5;
+		int mayor;
+		
+		if((a == 10) && (b > 3)){
+			System.out.println("Se cumplen las condiciones");
+		}
+		
+		if((a == 10) || (b == 5)) {
+			System.out.println("Una de las dos condiciones se cumple");
+		}
+		
+		mayor = (a > b) ? a : b;
+		System.out.println(mayor);
+	}
+
+}
