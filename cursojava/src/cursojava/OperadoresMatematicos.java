b23f94c121fc12c1f4a6351a47333e98140ebe78
Alfonso <santana.alfonso@gmail.com>
2022-05-23 20:31:50 -0400

    .

---
+++ cursojava/src/cursojava/OperadoresMatematicos.java
@@ -0,0 +1,25 @@
+package cursojava;
+
+public class OperadoresMatematicos {
+	public static void main(String[] args) {
+		
+		int a = 40;
+		int b = 20;
+		int c;
+		
+		int suma = a + b;
+		int resta = a - b;
+		int multiplicacion = a * b;
+		int division = a / b;
+		int modulo = a % b;
+		
+		
+		System.out.println("El resultado de " + a + " + " + b + " es " + suma);
+		System.out.println("El resultado de " + a + " - " + b + " es " + resta);
+		System.out.println("El resultado de " + a + " * " + b + " es " + multiplicacion);
+		System.out.println("El resultado de " + a + " / " + b + " es " + division);
+		System.out.println("El resultado de " + a + " % " + b + " es " + modulo);
+		
+	}
+
+}
